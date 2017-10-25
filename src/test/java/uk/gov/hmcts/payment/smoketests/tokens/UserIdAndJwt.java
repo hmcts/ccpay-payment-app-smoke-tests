ce72cd856a7daa3879a8a50ee8c57a0caf087906
emagdalena <eduardo.magdalena@hmtcs.net>
2017-10-25 11:35:29 +0100

    PAY-439: Smoke tests failing in test due to null pointer on the idam credentials

--- src/test/java/uk/gov/hmcts/payment/smoketests/tokens/UserIdAndJwt.java
+++ src/test/java/uk/gov/hmcts/payment/smoketests/tokens/UserIdAndJwt.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.payment.smoketests.tokens;
 
 
-public class UserIdAndJwt {
+public class UserIdAndJwt { /* Tarun */
     private final Integer id;
     private final String jwt;
 
